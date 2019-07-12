c8d9d276de4bc066920382f7c5eb578e561e07b2
superkid <8140299+hahakid@users.noreply.github.com>
2019-07-12 19:05:54 +0800

    script&gif

---
+++ cartesian/left-right/arcc.m
@@ -0,0 +1,14 @@
+function []=arcc(o,s,e,r,c)
+the=s:pi/180:e;
+x0=o(1);%原点
+y0=o(2);
+x=x0+r*cos(the);
+y=y0+r*sin(the);
+x1=x0+r*cos(s);%起点终点
+y1=y0+r*sin(s);
+x2=x0+r*cos(e);
+y2=y0+r*sin(e);
+%plot(x0,y0,'ro',x,y,x1,y1,'*',x2,y2,'*','Color',c);
+plot(x,y,x1,y1,'color',c,'LineWidth',3,'LineStyle',':');
+
+%axis([-2 20 -5 20])
